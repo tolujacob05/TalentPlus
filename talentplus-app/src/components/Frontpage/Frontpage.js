2f20bb22024c5f5a50d2da7363cab7b0dd4970c4
tolujacob05 <tolujacob05@gmail.com>
2023-03-23 22:08:18 +0100

    work-in-progress

--- talentplus-app/src/components/Frontpage/Frontpage.js
+++ talentplus-app/src/components/Frontpage/Frontpage.js
@@ -1,16 +1,36 @@
 import React from "react";
 import "./Frontpage.css";
 import arrow from "../../img/arrow.png";
-import bulb from "../../img/bulb.png";
-import Frame88 from "../../img/Frame88.png";
-import hero from "../../img/hero.png";
+import Hero from "../hero-image/Hero"
+// import Frame95 from "../../img/Frame95.png";
+// import Frame96 from "../../img/Frame96.png";
+// import Frame97 from "../../img/Frame97.png";
+// import Frame98 from "../../img/Frame98.png";
+// import Frame99 from "../../img/Frame99.png";
+import Frame100 from "../../img/Frame100.png";
+import Vector11 from "../../img/Vector11.png";
+import Vector3 from "../../img/Vector3.png";
+import Vector4 from "../../img/Vector4.png";
+
 
 const Frontpage = () => {
     return (
         <>
             <main>
+                <div className="vector11">
+                    <img src={Vector11} alt="" />
+                </div>
+                
                 <section className="first-section">
                     <div className="main-sec">
+                        <div className="dots">
+                            <div>
+                                <img src={Vector3} alt="" />
+                            </div>
+                            <div className="dot1">
+                                <img src={Vector4} alt="" />
+                            </div>
+                        </div>
                         <span>Grow your skills to advance your career path</span>
                         <span>build your future with our quality education. the best and largest all-in-one online tutoring platform in the world</span>
                         <div className="btn">
@@ -23,20 +43,21 @@ const Frontpage = () => {
                                 <button>enroll now</button>
                             </div>
                         </div>
-                    </div>
+                        <div className="main-images">
+                            <img src={Frame100} alt="" />
+                            {/* <img src={Frame96} alt="" />
+                            <img src={Frame97} alt="" />
+                            <img src={Frame98} alt="" />
+                            <img src={Frame99} alt="" /> */}
 
-                    <div className="images-section">
-                        <div className="img-sect1">
-                            <div className="bulb">
-                                <img src={bulb} alt="" />
-                            </div>
-                            <div className="figma">
-                                <img src={Frame88} alt="" />
+                            <div className="preview">
+                                <span>255k+</span>
+                                <span>Previews</span>
                             </div>
                         </div>
-                        <div className="hero-img">
-                            <img src={hero} alt="" />
-                        </div>
+                    </div>
+                    <div>
+                        <Hero />
                     </div>
                 </section>
             </main>
---
+++ talentplus-app/src/components/hero-image/Hero.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Hero.css";
+import bulb from "../../img/bulb.png";
+import Frame88 from "../../img/Frame88.png";
+import hero from "../../img/hero.png";
+import Vector12 from "../../img/Vector12.png";
+import Vector3 from "../../img/Vector3.png";
+import Vector4 from "../../img/Vector4.png";
+
+const Hero = () => {
+    return (
+        <>
+            <div className="images-section">
+                <div className="img-sect1">
+                    <div className="bulb">
+                        <img src={bulb} alt="" />
+                        <div className="dot2">
+                            <img src={Vector3} alt="" />
+                        </div>
+                    </div>
+                    <div className="figma">
+                        <img src={Vector12} alt="" />
+                        <div className="line">
+                            <img src={Frame88} alt="" />
+                        </div>
+                    </div>
+                    <div className="vs-code"></div>
+                    <div className="dot3">
+                        <img src={Vector4} alt="" />
+                    </div>
+                    <div className="hero-img">
+                        <img src={hero} alt="" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Hero;
