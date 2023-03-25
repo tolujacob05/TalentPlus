6795d1f510ede2d2106322efe6a1ca57365a2317
tolujacob05 <tolujacob05@gmail.com>
2023-03-25 03:57:20 +0100

    progress

--- talentplus-app/src/App.js
+++ talentplus-app/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
 import Class from "./components/classes/Class";
+import Course from "./components/courses/Course";
 import Frontpage from "./components/Frontpage/Frontpage";
 import Logo from "./components/logos/Logo";
 import Navbar from "./components/Navbar/Navbar";
+import Reason from "./components/reason/Reason";
 
 
 const App = () => {
@@ -14,6 +16,8 @@ const App = () => {
         <Frontpage />
         <Logo />
         <Class />
+        <Reason />
+        <Course />
       </div>
     </>
   )
---
+++ talentplus-app/src/components/reason/Reason.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "./Reason.css";
+import pic3 from "../../img/pic3.png";
+import Frame131 from "../../img/Frame131.png";
+import Frame132 from "../../img/Frame132.png";
+import Frame133 from "../../img/Frame133.png";
+import Frame134 from "../../img/Frame134.png";
+import Vector9 from "../../img/Vector9.png";
+import Vector14 from "../../img/Vector14.png";
+
+const Reason = () => {
+    return (
+        <>
+            <div className="reason-cont ">
+                <div className="reason-box">
+                    <span>this is why we are best from others</span>
+                    <span>high-defination video is video of higher resolution and quality than standard definition. while there’s no standard meaning for high definition, generally any standard video image</span>
+                    <div className="vector9">
+                        <img src={Vector9} alt="" />
+                    </div>
+                    <img src={pic3} alt="" />
+                </div>
+                <div className="reason-grid">
+                    <div className="grid">
+                        <div className="grid-img">
+                            <img src={Frame131} alt="" />
+                        </div>
+                        <div className="span">
+                            <span>experienced mentors</span>
+                            <span>high-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. </span>
+                        </div>
+                    </div>
+                    <div className="grid">
+                        <div className="grid-img">
+                            <img src={Frame132} alt="" />
+                        </div>
+                        <div className="span">
+                            <span>experienced mentors</span>
+                            <span>high-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. </span>
+                        </div>
+                    </div>
+                    <div className="grid">
+                        <div className="grid-img">
+                            <img src={Frame133} alt="" />
+                        </div>
+                        <div className="span">
+                            <span>experienced mentors</span>
+                            <span>high-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. </span>
+                        </div>
+                    </div>
+                    <div className="grid">
+                        <div className="grid-img">
+                            <img src={Frame134} alt="" />
+                        </div>
+                        <div className="span">
+                            <span>experienced mentors</span>
+                            <span>high-defination video is video of higher resolution and quality than standard definition. high-defination video is video of higher resolution and quality than standard definition. </span>
+                        </div>
+                    </div>
+
+                    <div className="vector14">
+                        <img src={Vector14} alt="" />
+                    </div>
+                </div>
+
+                
+            </div>
+        </>
+    )
+}
+
+export default Reason;
